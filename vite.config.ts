4144da65abebb9fb95ebfc16dd5b17aeac92b537
00010504 <wiut00010504@gmail.com>
2023-06-14 11:56:41 +0500

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      port: 5050,
+    },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    build: {
+      sourcemap: true,
+    },
+  };
+});
