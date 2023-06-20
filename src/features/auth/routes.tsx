a0ea45e5cb81ee9f927dfaa65c9bf3b54daac288
dilshodwiut <ramatilloyevdilshod7.0@gmail.com>
2023-06-20 16:15:38 +0500

    added page generator example

--- src/features/auth/routes.tsx
+++ src/features/auth/routes.tsx
@@ -1,11 +1,21 @@
 import type { CustomRoute } from "@/types";
+import { Outlet } from "react-router-dom";
+
+// VIEW IMPORTS
 
 const routes: CustomRoute = {
   id: "auth",
   title: "Auth",
   path: "auth",
-  element: <div>auth route</div>,
-  children: [],
+  element: (
+    <>
+      <div>auth route</div>
+      <Outlet />
+    </>
+  ),
+  children: [
+    // ROUTES
+  ],
 };
 
 export default routes;
